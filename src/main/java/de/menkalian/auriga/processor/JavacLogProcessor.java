1b3367f3bc916de15e41513f0961eaa04614b510
Kilian Krampf <kiliankra@gmail.com>
2020-12-23 02:27:07 +0100

    specialized JavacLogProcessor extracted

---
+++ src/main/java/de/menkalian/auriga/processor/JavacLogProcessor.java
@@ -0,0 +1,95 @@
+package de.menkalian.auriga.processor;
+
+import com.sun.tools.javac.model.JavacElements;
+import com.sun.tools.javac.processing.JavacProcessingEnvironment;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.TreeMaker;
+import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.Name;
+import de.menkalian.auriga.Config;
+import de.menkalian.auriga.annotations.Log;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+public class JavacLogProcessor extends AbstractProcessor {
+    JavacProcessingEnvironment processingEnvironment;
+    TreeMaker instance;
+    JavacElements elementUtils;
+    Config config;
+
+    @Override
+    public boolean process (Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        if (processingEnvironment != null) {
+            Set<? extends Element> methods = roundEnv.getElementsAnnotatedWith(Log.class);
+            for (Element method : methods) {
+                generateHeaderLogsForMethod(method);
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public synchronized void init (ProcessingEnvironment processingEnv) {
+        processingEnvironment = (JavacProcessingEnvironment) processingEnv;
+
+        elementUtils = processingEnvironment.getElementUtils();
+        instance = TreeMaker.instance(processingEnvironment.getContext());
+
+        // Load config
+        if (processingEnvironment.getOptions().containsKey("Auriga.Config.File")) {
+            config = new Config(processingEnvironment.getOptions().get("Auriga.Config.File"));
+        } else {
+            config = new Config();
+        }
+    }
+
+    private void generateHeaderLogsForMethod (Element method) {
+        final JCTree.JCMethodDecl tree = (JCTree.JCMethodDecl) elementUtils.getTree(method);
+        final List<JCTree.JCStatement> stats = tree.body.stats;
+        tree.body.stats = stats.prepend(
+                instance.Exec(instance.Apply(null, convertStringToJC("System.out.printf"), generateArgumentsFromMethod(method)))
+        );
+    }
+
+    private JCTree.JCExpression convertStringToJC (String ref) {
+        final String[] split = ref.split("\\.");
+
+        Name name = elementUtils.getName(split[0]);
+        JCTree.JCExpression toReturn = instance.Ident(name);
+
+        for (int i = 1 ; i < split.length ; i++) {
+            name = elementUtils.getName(split[i]);
+            toReturn = instance.Select(toReturn, name);
+        }
+
+        return toReturn;
+    }
+
+    private List<JCTree.JCExpression> generateArgumentsFromMethod (Element method) {
+        final JCTree.JCMethodDecl tree = (JCTree.JCMethodDecl) elementUtils.getTree(method);
+        StringBuilder formatStringParameter = new StringBuilder();
+        List<JCTree.JCExpression> formatParameters = List.nil();
+
+        formatStringParameter.append("Executing Method: ").append(method.getSimpleName()).append("%n");
+
+        for (JCTree.JCVariableDecl parameter : tree.getParameters()) {
+            formatStringParameter.append("Parameter ").append(parameter.name.toString()).append(": %s%n");
+            if (parameter.vartype instanceof JCTree.JCArrayTypeTree) {
+                formatParameters = formatParameters.append(instance.Apply(null, instance.Select(convertStringToJC("java.util.Arrays"), elementUtils.getName("toString")), List.of(instance.Ident(parameter))));
+            } else if (parameter.vartype instanceof JCTree.JCPrimitiveTypeTree) {
+                formatParameters = formatParameters.append(instance.Apply(null, instance.Select(convertStringToJC("String"), elementUtils.getName("valueOf")), List.of(instance.Ident(parameter))));
+            } else {
+                formatParameters = formatParameters.append(instance.Apply(null, instance.Select(instance.Ident(parameter), elementUtils.getName("toString")), List.nil()));
+            }
+        }
+        return formatParameters.prepend(instance.Literal(formatStringParameter.toString()));
+    }
+
+}
--- src/main/java/de/menkalian/auriga/processor/LogProcessor.java
+++ src/main/java/de/menkalian/auriga/processor/LogProcessor.java
@@ -1,13 +1,6 @@
 package de.menkalian.auriga.processor;
 
-import com.sun.tools.javac.model.JavacElements;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
-import com.sun.tools.javac.tree.JCTree;
-import com.sun.tools.javac.tree.TreeMaker;
-import com.sun.tools.javac.util.List;
-import com.sun.tools.javac.util.Name;
-import de.menkalian.auriga.Config;
-import de.menkalian.auriga.annotations.Log;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
@@ -15,7 +8,6 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.util.Set;
@@ -23,27 +15,14 @@ import java.util.Set;
 @SupportedAnnotationTypes("de.menkalian.auriga.annotations.*")
 @SupportedSourceVersion(SourceVersion.RELEASE_15)
 public class LogProcessor extends AbstractProcessor {
-    JavacProcessingEnvironment processingEnvironment;
-    TreeMaker instance;
-    JavacElements elementUtils;
-    Config config;
+    AbstractProcessor specializedProcessor;
 
     @Override
     public synchronized void init (ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
         if (processingEnv instanceof JavacProcessingEnvironment) {
-            processingEnvironment = (JavacProcessingEnvironment) processingEnv;
-
-            elementUtils = processingEnvironment.getElementUtils();
-            instance = TreeMaker.instance(processingEnvironment.getContext());
-
-            // Load config
-            if (processingEnvironment.getOptions().containsKey("Auriga.Config.File")) {
-                config = new Config(processingEnvironment.getOptions().get("Auriga.Config.File"));
-            } else {
-                config = new Config();
-            }
-            System.out.println("Loaded Config: " + config);
+            specializedProcessor = new JavacLogProcessor();
+            specializedProcessor.init(processingEnv);
         } else {
             processingEnv.getMessager().printMessage(Diagnostic.Kind.MANDATORY_WARNING, "Unknown Processing-Environment detected! Auriga does not work here properly! Detected Environment: " + processingEnv.getClass().getCanonicalName());
         }
@@ -51,56 +30,10 @@ public class LogProcessor extends AbstractProcessor {
 
     @Override
     public boolean process (Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        if (processingEnvironment != null) {
-            Set<? extends Element> methods = roundEnv.getElementsAnnotatedWith(Log.class);
-            for (Element method : methods) {
-                generateHeaderLogsForMethod(method);
-            }
-            return true;
+        if (specializedProcessor == null) {
+            return false;
+        } else {
+            return specializedProcessor.process(annotations, roundEnv);
         }
-
-        return false;
-    }
-
-    private void generateHeaderLogsForMethod (Element method) {
-        final JCTree.JCMethodDecl tree = (JCTree.JCMethodDecl) elementUtils.getTree(method);
-        final List<JCTree.JCStatement> stats = tree.body.stats;
-        tree.body.stats = stats.prepend(
-                instance.Exec(instance.Apply(null, convertStringToJC("System.out.printf"), generateArgumentsFromMethod(method)))
-        );
-    }
-
-    private JCTree.JCExpression convertStringToJC (String ref) {
-        final String[] split = ref.split("\\.");
-
-        Name name = elementUtils.getName(split[0]);
-        JCTree.JCExpression toReturn = instance.Ident(name);
-
-        for (int i = 1 ; i < split.length ; i++) {
-            name = elementUtils.getName(split[i]);
-            toReturn = instance.Select(toReturn, name);
-        }
-
-        return toReturn;
-    }
-
-    private List<JCTree.JCExpression> generateArgumentsFromMethod (Element method) {
-        final JCTree.JCMethodDecl tree = (JCTree.JCMethodDecl) elementUtils.getTree(method);
-        StringBuilder formatStringParameter = new StringBuilder();
-        List<JCTree.JCExpression> formatParameters = List.nil();
-
-        formatStringParameter.append("Executing Method: ").append(method.getSimpleName()).append("%n");
-
-        for (JCTree.JCVariableDecl parameter : tree.getParameters()) {
-            formatStringParameter.append("Parameter ").append(parameter.name.toString()).append(": %s%n");
-            if (parameter.vartype instanceof JCTree.JCArrayTypeTree) {
-                formatParameters = formatParameters.append(instance.Apply(null, instance.Select(convertStringToJC("java.util.Arrays"), elementUtils.getName("toString")), List.of(instance.Ident(parameter))));
-            } else if (parameter.vartype instanceof JCTree.JCPrimitiveTypeTree) {
-                formatParameters = formatParameters.append(instance.Apply(null, instance.Select(convertStringToJC("String"), elementUtils.getName("valueOf")), List.of(instance.Ident(parameter))));
-            } else {
-                formatParameters = formatParameters.append(instance.Apply(null, instance.Select(instance.Ident(parameter), elementUtils.getName("toString")), List.nil()));
-            }
-        }
-        return formatParameters.prepend(instance.Literal(formatStringParameter.toString()));
     }
 }
