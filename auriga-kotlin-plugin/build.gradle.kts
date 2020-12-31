a7792caac676b178d5e4a2c581c2de4b784f9776
Kilian Krampf <kiliankra@gmail.com>
2020-12-31 02:30:34 +0100

    CI fixing (still)

--- auriga-kotlin-plugin/build.gradle.kts
+++ auriga-kotlin-plugin/build.gradle.kts
@@ -31,8 +31,14 @@ publishing {
     }
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+    }
+}
 kapt {
-    includeCompileClasspath=false
+    includeCompileClasspath = false
 }
 
 dependencies {
