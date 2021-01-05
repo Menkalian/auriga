73126bca267cc285907a96b5930cc553bcd1634d
Menkalian <kilian@krampf.de>
2021-01-05 23:14:57 +0100

    Fix dependent configurations for other plugins (java, kotlin)

--- auriga-gradle-plugin/src/main/kotlin/de/menkalian/auriga/AurigaGradlePlugin.kt
+++ auriga-gradle-plugin/src/main/kotlin/de/menkalian/auriga/AurigaGradlePlugin.kt
@@ -1,61 +1,88 @@
+@file:Suppress("unused")
+
 package de.menkalian.auriga
 
 import de.menkalian.auriga.config.Auriga
 import de.menkalian.auriga.config.AurigaLoggerConfig
 import de.menkalian.auriga.config.AurigaLoggingConfig
-import org.gradle.api.NamedDomainObjectContainer
+import groovy.lang.Closure
 import org.gradle.api.Project
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.compile.JavaCompile
 import java.net.URI
-import javax.inject.Inject
+import java.util.concurrent.atomic.AtomicBoolean
 
 class AurigaGradlePlugin : org.gradle.api.Plugin<Project> {
-    override fun apply(project: Project) {
-        // Create the configuration space
-        project.extensions.create(
-            "auriga", AurigaGradleExtension::class.java
-        )
-        // Add the Annotations dependency
-        project.dependencies.add("implementation", "de.menkalian.auriga:auriga-annotations:1.0.0")
+    private val baseApplied = AtomicBoolean(false)
 
-        // Add the repository if needed
-        if (project.repositories.any {
-                if (it is MavenArtifactRepository) {
-                    it.url.toString() == "http://server.menkalian.de:8081/artifactory/auriga"
-                } else false
-            }) {
-            project.repositories.maven {
-                it.name = "artifactory-auriga-menkalian"
-                it.url = URI("http://server.menkalian.de:8081/artifactory/auriga")
-            }
+    override fun apply(project: Project) {
+        project.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+            applyBaseIfNotAlreadyApplied(project)
         }
 
         // Check if java is present
-        if (project.pluginManager.hasPlugin("java")) {
-            project.dependencies.add("annotationProcessor", "de.menkalian.auriga:auriga-java-processor:1.0.0")
+        project.pluginManager.withPlugin("java") {
+            applyJavaParams(project)
+        }
 
-            val gradleExtension = project.extensions.getByType(AurigaGradleExtension::class.java)
+        // Android has java as well
+        project.pluginManager.withPlugin("com.android.application") {
+            applyJavaParams(project)
+        }
+        project.pluginManager.withPlugin("com.android.library") {
+            applyJavaParams(project)
+        }
+    }
 
-            addJavacOption(project, Auriga.Config.base, gradleExtension.base)
-            addJavacOption(project, Auriga.Config.location, gradleExtension.location)
-            addJavacOption(project, Auriga.Config.type, gradleExtension.type)
+    private fun applyJavaParams(project: Project) {
+        applyBaseIfNotAlreadyApplied(project)
 
-            addJavacOption(project, Auriga.Logging.method, gradleExtension.logging.method)
-            addJavacOption(project, Auriga.Logging.mode, gradleExtension.logging.mode)
-            addJavacOption(project, Auriga.Logging.placeholder, gradleExtension.logging.placeholder)
-            addJavacOption(project, Auriga.Logging.Template.entry, gradleExtension.logging.entryTemplate)
-            addJavacOption(project, Auriga.Logging.Template.param, gradleExtension.logging.paramTemplate)
-            addJavacOption(project, Auriga.Logger.type, gradleExtension.logger.type)
-            addJavacOption(project, Auriga.Logger.clazz, gradleExtension.logger.clazz)
-            addJavacOption(project, Auriga.Logger.source, gradleExtension.logger.source)
+        project.dependencies.add("annotationProcessor", "de.menkalian.auriga:auriga-java-processor:1.0.0")
+
+        val gradleExtension = project.extensions.getByType(AurigaGradleExtension::class.java)
+        println(gradleExtension)
+
+        addJavacOption(project, Auriga.Config.base, gradleExtension.base)
+        addJavacOption(project, Auriga.Config.location, gradleExtension.location)
+        addJavacOption(project, Auriga.Config.type, gradleExtension.type)
+
+        addJavacOption(project, Auriga.Logging.method, gradleExtension.loggingConfig.method)
+        addJavacOption(project, Auriga.Logging.mode, gradleExtension.loggingConfig.mode)
+        addJavacOption(project, Auriga.Logging.placeholder, gradleExtension.loggingConfig.placeholder)
+        addJavacOption(project, Auriga.Logging.Template.entry, gradleExtension.loggingConfig.entryTemplate)
+        addJavacOption(project, Auriga.Logging.Template.param, gradleExtension.loggingConfig.paramTemplate)
+        addJavacOption(project, Auriga.Logger.type, gradleExtension.loggerConfig.type)
+        addJavacOption(project, Auriga.Logger.clazz, gradleExtension.loggerConfig.clazz)
+        addJavacOption(project, Auriga.Logger.source, gradleExtension.loggerConfig.source)
+    }
+
+    private fun applyBaseIfNotAlreadyApplied(project: Project) {
+        if(!baseApplied.getAndSet(true)) {
+            // Create the configuration space
+            project.extensions.create(
+                "auriga", AurigaGradleExtension::class.java, project
+            )
+
+            // Add the Annotations dependency
+            project.dependencies.add("implementation", "de.menkalian.auriga:auriga-annotations:1.0.0")
+
+            // Add the repository if needed
+            if (project.repositories.any {
+                    if (it is MavenArtifactRepository) {
+                        it.url.toString() == "http://server.menkalian.de:8081/artifactory/auriga"
+                    } else false
+                }) {
+                project.repositories.maven {
+                    it.name = "artifactory-auriga-menkalian"
+                    it.url = URI("http://server.menkalian.de:8081/artifactory/auriga")
+                }
+            }
         }
     }
 
     private fun addJavacOption(project: Project, key: String, value: String) {
         project.tasks.withType(JavaCompile::class.java).all {
-            if(value.isNotBlank()){
+            if (value.isNotBlank()) {
                 project.logger.lifecycle("Adding $key -> $value")
                 it.options.compilerArgs.add("-A$key=$value")
             }
@@ -63,12 +90,26 @@ class AurigaGradlePlugin : org.gradle.api.Plugin<Project> {
     }
 }
 
-
-open class AurigaGradleExtension @Inject constructor(objectFactory: ObjectFactory) {
+open class AurigaGradleExtension constructor(val project: Project) {
     open var base: String = ""
     open var type: String = "ARGS"
     open var location: String = ""
 
-    open var logging: AurigaLoggingConfig = objectFactory.newInstance(AurigaLoggingConfig::class.java)
-    open var logger: AurigaLoggerConfig = objectFactory.newInstance(AurigaLoggerConfig::class.java)
+    open var loggingConfig: AurigaLoggingConfig = AurigaLoggingConfig()
+    open var loggerConfig: AurigaLoggerConfig = AurigaLoggerConfig()
+
+    open fun loggingConfig(closure: Closure<AurigaLoggingConfig>): AurigaLoggingConfig {
+        project.configure(loggingConfig, closure)
+        return loggingConfig
+    }
+
+    open fun loggerConfig(closure: Closure<AurigaLoggerConfig>): AurigaLoggerConfig {
+        project.configure(loggerConfig, closure)
+        return loggerConfig
+    }
+
+    override fun toString(): String {
+        return "AurigaGradleExtension(project=$project, base='$base', type='$type', location='$location', loggingConfig=$loggingConfig, loggerConfig=$loggerConfig)"
+    }
+
 }
