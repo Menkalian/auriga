4f4de6e7355a19baa68bae3aeafe892702ac28f9
Kilian Krampf <kiliankra@gmail.com>
2021-01-10 17:58:21 +0100

    Update Compiler Plugin to read configuration

--- auriga-kotlin-plugin/src/main/kotlin/de/menkalian/auriga/AurigaConfigurationKeys.kt
+++ auriga-kotlin-plugin/src/main/kotlin/de/menkalian/auriga/AurigaConfigurationKeys.kt
@@ -3,6 +3,18 @@ package de.menkalian.auriga
 import org.jetbrains.kotlin.config.CompilerConfigurationKey
 
 object AurigaConfigurationKeys {
-    val KEY_ENABLED = CompilerConfigurationKey.create<Boolean>("enabled state")
-    val KEY_ANNOTATION = CompilerConfigurationKey.create<List<String>>("annotation qualified name")
+    val KEY_CONFIG_TYPE = CompilerConfigurationKey.create<String>("type of configuration")
+    val KEY_CONFIG_LOCATION = CompilerConfigurationKey.create<String>("location of configuration")
+    val KEY_CONFIG_BASE = CompilerConfigurationKey.create<String>("base configuration")
+
+    val KEY_LOGGER_TYPE = CompilerConfigurationKey.create<String>("type of logger")
+    val KEY_LOGGER_CLAZZ = CompilerConfigurationKey.create<String>("class of logger")
+    val KEY_LOGGER_SOURCE = CompilerConfigurationKey.create<String>("source of logger")
+
+    val KEY_LOGGING_MODE = CompilerConfigurationKey.create<String>("mode of logging")
+    val KEY_LOGGING_METHOD = CompilerConfigurationKey.create<String>("method for logging")
+    val KEY_LOGGING_PLACEHOLDER = CompilerConfigurationKey.create<String>("placeholder for logging calls")
+
+    val KEY_LOGGING_TEMPLATE_ENTRY = CompilerConfigurationKey.create<String>("template for logging method entries")
+    val KEY_LOGGING_TEMPLATE_PARAM = CompilerConfigurationKey.create<String>("template for logging method parameters")
 }
