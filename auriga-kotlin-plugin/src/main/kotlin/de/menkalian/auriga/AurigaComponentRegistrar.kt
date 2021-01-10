11a0fdcd0e6a8c41c80808db2f467aa9a686499c
Kilian Krampf <kiliankra@gmail.com>
2021-01-10 18:17:52 +0100

    Load AurigaConfig from (kotlin) CompilerConfig

--- auriga-kotlin-plugin/src/main/kotlin/de/menkalian/auriga/AurigaComponentRegistrar.kt
+++ auriga-kotlin-plugin/src/main/kotlin/de/menkalian/auriga/AurigaComponentRegistrar.kt
@@ -1,18 +1,42 @@
 package de.menkalian.auriga
 
 import com.google.auto.service.AutoService
+import de.menkalian.auriga.config.Auriga
+import de.menkalian.auriga.config.AurigaConfig
 import org.jetbrains.kotlin.codegen.extensions.ClassBuilderInterceptorExtension
 import org.jetbrains.kotlin.com.intellij.mock.MockProject
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.CompilerConfigurationKey
 
 @AutoService(ComponentRegistrar::class)
 class AurigaComponentRegistrar : ComponentRegistrar {
     override fun registerProjectComponents(project: MockProject, configuration: CompilerConfiguration) {
+        val mapFromCompilerConfig = getMapFromCompilerConfig(configuration)
+        val config = AurigaConfig(mapFromCompilerConfig)
+        org.jetbrains.kotlin.konan.file.createTempFile("AURIGA").writeText(config.toString())
         ClassBuilderInterceptorExtension.registerExtension(
             project, AurigaClassGenerationInterceptor(
-                configuration[AurigaConfigurationKeys.KEY_ANNOTATION] ?: error("Auriga needs!")
+                listOf()
             )
         )
+    }
+
+    private fun getMapFromCompilerConfig(configuration: CompilerConfiguration): Map<Any, Any> {
+        val toReturn = mutableMapOf<Any, Any>()
+
+        Auriga.getKeys().forEach { key ->
+            val configKey = AurigaConfigurationKeys::class.members.filter {
+                it.name.equals(
+                    "KEY_" + key.substring("auriga.".length).toUpperCase().replace('.', '_')
+                )
+            }.first().call(AurigaConfigurationKeys) as? CompilerConfigurationKey<*> ?: return@forEach
+
+            val value = configuration.get(configKey)
+            if (value != null)
+                toReturn[key] = value
+        }
+
+        return toReturn
     }
 }
