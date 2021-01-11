c40cc7262d9004a11e33edb744badbdd7aa94edb
Kilian Krampf <kiliankra@gmail.com>
2021-01-11 16:59:20 +0100

    Basic Log Implementation

--- auriga-kotlin-plugin/src/main/kotlin/de/menkalian/auriga/AurigaClassBuilder.kt
+++ auriga-kotlin-plugin/src/main/kotlin/de/menkalian/auriga/AurigaClassBuilder.kt
@@ -1,41 +1,84 @@
 package de.menkalian.auriga
 
 import de.menkalian.auriga.config.AurigaConfig
+import de.menkalian.auriga.config.Placeholder
 import org.jetbrains.kotlin.codegen.ClassBuilder
 import org.jetbrains.kotlin.codegen.DelegatingClassBuilder
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.resolve.descriptorUtil.parents
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOrigin
 import org.jetbrains.org.objectweb.asm.MethodVisitor
 import org.jetbrains.org.objectweb.asm.Opcodes
+import org.jetbrains.org.objectweb.asm.Type
 import org.jetbrains.org.objectweb.asm.commons.InstructionAdapter
 
-class AurigaClassBuilder(val config: AurigaConfig, val delegateBuilder: ClassBuilder) : DelegatingClassBuilder() {
+class AurigaClassBuilder(val config: AurigaConfig, private val delegateBuilder: ClassBuilder) : DelegatingClassBuilder() {
     override fun getDelegate(): ClassBuilder = delegateBuilder
-
-    init {
-        org.jetbrains.kotlin.konan.file.createTempFile("AURIGA", ".txt").writeText("${delegateBuilder.thisName}\n$config")
-    }
 
     override fun newMethod(origin: JvmDeclarationOrigin, access: Int, name: String, desc: String, signature: String?, exceptions: Array<out String>?
     ): MethodVisitor {
         val original = super.newMethod(origin, access, name, desc, signature, exceptions)
         val function = origin.descriptor as? FunctionDescriptor ?: return original
-        org.jetbrains.kotlin.konan.file.createTempFile(function.name.toString(), ".method.txt").writeText(function.toString())
+
+//        val modifyMethod = when (config.loggingConfig.mode) {
+//            "DEFAULT_OFF" -> function.parentsWithSelf.any { it.annotations.hasAnnotation(FqName("de.menkalian.auriga.Log")) }
+//            "DEFAULT_ON"  -> function.parentsWithSelf.none { it.annotations.hasAnnotation(FqName("de.menkalian.auriga.NoLog")) }
+//            else          -> error("Unknown Logging Mode")
+//        }
+
+//        if (modifyMethod) return original
         return object : MethodVisitor(Opcodes.ASM5, original) {
             override fun visitCode() {
                 super.visitCode()
                 InstructionAdapter(this).apply {
-                    TODO("on method entry")
-                }
-            }
+                    getstatic("java/lang/System", "out", "Ljava/io/PrintStream;")
+                    anew(Type.getType(StringBuilder::class.java))
+                    dup()
+                    invokespecial("java/lang/StringBuilder", "<init>", "()V", false)
 
-            override fun visitInsn(opcode: Int) {
-                when (opcode) {
-                    Opcodes.RETURN, Opcodes.ARETURN, Opcodes.IRETURN -> {
-                        InstructionAdapter(this).apply { TODO("on method exit") }
+                    val template = config.loggingConfig.entryTemplate
+                        .replace(Placeholder.METHOD, function.name.toString())
+                        .replace(Placeholder.CLASS, function.parents.first().name.toString())
+
+                    val templateSplit = template.split("{{")
+                    templateSplit.forEach {
+                        it.split("}}").forEach {
+                            when (it) {
+                                "THIS"   -> {
+                                    visitVarInsn(Opcodes.ALOAD, -1)
+                                    invokevirtual("java/lang/Object", "toString", "()Ljava/lang/String;", false)
+                                    invokevirtual("java/lang/StringBuilder", "append", "(Ljava/lang/Object;)Ljava/lang/StringBuilder;", false)
+                                }
+                                "PARAMS" -> {
+                                    function.valueParameters.forEachIndexed { i, parameterDescriptor ->
+                                        val paramTemplate = config.loggingConfig.paramTemplate
+                                            .replace(Placeholder.PARAM_NAME, parameterDescriptor.name.identifier)
+                                            .replace(Placeholder.PARAM_TYPE, parameterDescriptor.type.toString())
+                                        val paramTemplateSplit = paramTemplate.split(Regex.fromLiteral("(\\{\\{)|(\\}\\})"))
+                                        paramTemplateSplit.forEach {
+                                            when (it) {
+                                                "PARAM_VALUE" -> {
+                                                    visitVarInsn(Opcodes.ALOAD, i + 1)
+                                                    invokevirtual("java/lang/StringBuilder", "append", "(Ljava/lang/Object;)Ljava/lang/StringBuilder;", false)
+                                                }
+                                                else          -> {
+                                                    visitLdcInsn(it)
+                                                    invokevirtual("java/lang/StringBuilder", "append", "(Ljava/lang/Object;)Ljava/lang/StringBuilder;", false)
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                else     -> {
+                                    visitLdcInsn(it)
+                                    invokevirtual("java/lang/StringBuilder", "append", "(Ljava/lang/Object;)Ljava/lang/StringBuilder;", false)
+                                }
+                            }
+                        }
                     }
+                    invokevirtual("java/lang/Object", "toString", "()Ljava/lang/String;", false)
+                    invokevirtual("java/io/PrintStream", "println", "(Ljava/lang/String;)V", false)
                 }
-                super.visitInsn(opcode)
             }
         }
     }
